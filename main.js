0f9cd51001dd27c6ed77e9bf9531cc5ab24e5726
Adriano Duarte <122303785+adriamano@users.noreply.github.com>
2023-02-01 20:12:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    console.log($('form button'));
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const adicionaTarefa = $('#escreve-tarefa').val();
+        const novaTarefa = $('<li style="display:none"></li>');
+        $(`<p>${adicionaTarefa}</p>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        $(novaTarefa).slideDown(500);
+        $('#escreve-tarefa').val('')
+    })
+    
+    $('li').click(function() {
+        $(this).addClass('item-completado');
+    });
+    
+
+})
